d222de4c3c7512663c0303028f061d28d67380eb
Dario Trombello <dariotr@outlook.de>
2021-11-02 17:04:19 +0100

    add substitution plan notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:engelsburg_app/src/provider/notification.dart';
 import 'package:engelsburg_app/src/provider/substitute.dart';
 import 'package:engelsburg_app/src/services/db_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,8 +12,10 @@ import 'src/services/shared_prefs.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await SharedPrefs.init();
   await DatabaseService.init();
+  await Firebase.initializeApp();
 
   runApp(
     MultiProvider(
